dabbe536529105b7fd09a943ed89a220f239896f
elesawy2 <elesawy928@gmail.com>
2024-10-08 15:11:27 +0300

    book_entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity{
+  final String image;
+  final String authorName;
+  final String title;
+  final num rate;
+  final num price;
+
+  BookEntity(this.image, this.authorName, this.title, this.rate, this.price);
+}
