a491073a76f1dc1e5f09839c98f5caabbf3ae80d
elesawy2 <elesawy928@gmail.com>
2024-10-08 15:26:52 +0300

    add either class

--- lib/Features/home/domain/repo/homr_repo.dart
+++ lib/Features/home/domain/repo/homr_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomrRepo{
-  Future<List<BookEntity>>fetchFeaturedBook();
-  Future<List<BookEntity>>fetchNewestBook();
+  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBook();
+  Future<Either<Failure,List<BookEntity>>>fetchNewestBook();
 }
